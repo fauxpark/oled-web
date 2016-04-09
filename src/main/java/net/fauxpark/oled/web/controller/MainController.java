1560d3e49e381c4941b8c0074d630e30383c5860
fauxpark <fauxpark@gmail.com>
2016-04-10 00:15:37 +1000

    Display version and git commit in footer

--- src/main/java/net/fauxpark/oled/web/controller/MainController.java
+++ src/main/java/net/fauxpark/oled/web/controller/MainController.java
@@ -2,8 +2,10 @@ package net.fauxpark.oled.web.controller;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * A controller which handles serving webapp pages.
@@ -15,13 +17,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MainController {
 	private static final Logger log = LogManager.getLogger(MainController.class);
 
+	@Value("${build.version}")
+	private String buildVersion;
+
+	@Value("${build.sha}")
+	private String buildSha;
+
 	/**
 	 * Show the index page.
 	 */
 	@RequestMapping("/")
-	public String index() {
+	public ModelAndView index() {
 		log.info("======== index");
 
-		return "index";
+		ModelAndView mav = new ModelAndView("index");
+		mav.addObject("buildVersion", buildVersion);
+		mav.addObject("buildSha", buildSha);
+
+		return mav;
 	}
 }
--- src/main/webapp/WEB-INF/pages/footer.jsp
+++ src/main/webapp/WEB-INF/pages/footer.jsp
@@ -1,5 +1,6 @@
 <%@taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt"%>
-<footer class="text-muted">
+<footer class="row text-muted">
 	<hr/>
-	<div class="pull-right">&copy; 2016 fauxpark</div>
+	<p class="pull-left">v${buildVersion}-${buildSha}</p>
+	<p class="pull-right">&copy; 2016 fauxpark</p>
 </footer>
