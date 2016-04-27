81bab460364621ebf4e71b3b731e65f14a4a8651
fauxpark <fauxpark@gmail.com>
2016-04-27 15:07:05 +1000

    Use JSON response to set state

--- src/main/webapp/resources/js/OledCtrl.js
+++ src/main/webapp/resources/js/OledCtrl.js
@@ -56,13 +56,13 @@ oled.controller('OledCtrl', ['$scope', 'StateService', 'PreviewService', 'Buffer
 				OledService.displayOff(function(response) {
 					console.log('Turned display off.');
 
-					$scope.state.displayOn = false;
+					$scope.state.displayOn = response.result;
 				});
 			} else {
 				OledService.displayOn(function(response) {
 					console.log('Turned display on.');
 
-					$scope.state.displayOn = true;
+					$scope.state.displayOn = response.result;
 				});
 			}
 		} else {
@@ -87,7 +87,7 @@ oled.controller('OledCtrl', ['$scope', 'StateService', 'PreviewService', 'Buffer
 			OledService.invert(function(response) {
 				console.log('Inverted display.');
 
-				$scope.state.inverted = !$scope.state.inverted;
+				$scope.state.inverted = response.result;
 			});
 		} else {
 			alert('Can\'t do anything while the display is not initialised.');
@@ -99,7 +99,7 @@ oled.controller('OledCtrl', ['$scope', 'StateService', 'PreviewService', 'Buffer
 			OledService.flip('h', function(response) {
 				console.log('Flipped display horizontally.');
 
-				$scope.state.hFlipped = !$scope.state.hFlipped;
+				$scope.state.hFlipped = response.result;
 			});
 		} else {
 			alert('Can\'t do anything while the display is not initialised.');
@@ -111,7 +111,7 @@ oled.controller('OledCtrl', ['$scope', 'StateService', 'PreviewService', 'Buffer
 			OledService.flip('v', function(response) {
 				console.log('Flipped display vertically.');
 
-				$scope.state.vFlipped = !$scope.state.vFlipped;
+				$scope.state.vFlipped = response.result;
 			});
 		} else {
 			alert('Can\'t do anything while the display is not initialised.');
@@ -137,13 +137,13 @@ oled.controller('OledCtrl', ['$scope', 'StateService', 'PreviewService', 'Buffer
 						console.log('Started scrolling rows ' + offset + ' to ' + (offset + rows) + ' vertically at ' + step + ' rows per step.');
 					}
 
-					$scope.state.scrolling = true;
+					$scope.state.scrolling = response.result;
 				});
 			} else {
 				OledService.stopScroll(function(response) {
 					console.log('Stopped scrolling.');
 
-					$scope.state.scrolling = false;
+					$scope.state.scrolling = response.result;
 				});
 			}
 		} else {
@@ -157,22 +157,6 @@ oled.controller('OledCtrl', ['$scope', 'StateService', 'PreviewService', 'Buffer
 
 			OledService.setContrast(contrast, function(response) {
 				console.log('Set contrast level to ' + contrast + '.');
-			});
-		} else {
-			alert('Can\'t do anything while the display is not initialised.');
-		}
-	};
-
-	$scope.setPixel = function() {
-		if($scope.state.initialised) {
-			var x = parseInt($('#input-pixel-x').val());
-			var y = parseInt($('#input-pixel-y').val());
-			var on = $('#input-pixel-on').hasClass('active');
-
-			BufferService.setPixel(x, y, on, function(response) {
-				console.log('Turned pixel at ' + x + ',' + y + ' ' + (on ? 'on' : 'off') + '.');
-
-				PreviewService.setPixel(x, y, on);
 			});
 		} else {
 			alert('Can\'t do anything while the display is not initialised.');
